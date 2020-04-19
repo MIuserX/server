c7856dd06137426e3f067ab1b5fa2a9c6701417a
MIuserX <jwdcool@foxmail.com>
2020-04-19 05:26:59 -0900

    add print for FIN

--- packet.c
+++ packet.c
@@ -13,6 +13,7 @@ void dumpPacket( Packet * p ) {
     printf("\n====\n");
     printf("PSH=%d x_seq=%u\n", p->head.flags&ACTION_PSH ? 1 : 0, p->head.x_seq );
     printf("ACK=%d x_ack=%u\n", p->head.flags&ACTION_ACK ? 1 : 0, p->head.x_ack );
+    printf("FIN=%d\n", p->head.flags&ACTION_FIN ? 1 : 0 );
     printf("sz=%u wnd=%u\n", p->head.sz, p->head.wnd );
     printf("data=|");
     for ( i = 0; i < p->head.sz - PACKET_HEAD_SZ; i++ ) {
